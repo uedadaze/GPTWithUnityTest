395c62bbc6aa709508eb9383c51963ea5906f209
Kirikabu <itsuki@julious.jp>
2023-03-26 15:47:15 +0900

    Add files via upload

--- ScriptGeneratorByGPT4/Editor/ScriptGeneratorWindow.cs
+++ ScriptGeneratorByGPT4/Editor/ScriptGeneratorWindow.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -13,6 +14,7 @@ public class ScriptGeneratorWindow : EditorWindow
     private string _question = string.Empty;
     private string _responseText = string.Empty;
     private bool _isRequestInProgress = false;
+    private bool _isScriptGenerated = false;
     private int _selectedModel = 0;
     private string[] _modelOptions = { "gpt-3.5-turbo", "gpt-4" };
     private const string ApiEndpoint = "https://api.openai.com/v1/chat/completions";
@@ -28,6 +30,7 @@ public class ScriptGeneratorWindow : EditorWindow
     private void OnEnable()
     {
         _isRequestInProgress = false;
+        _isScriptGenerated = false;
     }
 
     private void OnGUI()
@@ -39,22 +42,29 @@ public class ScriptGeneratorWindow : EditorWindow
         EditorGUILayout.LabelField("やらせたいことを入力して下さい:", EditorStyles.boldLabel);
         _question = EditorGUILayout.TextArea(_question, GUILayout.Height(100));
 
-        if (_isRequestInProgress)
-        {
+        if (_isRequestInProgress){
             GUI.enabled = false;
             GUILayout.Button("回答待機中");
             GUI.enabled = true;
-        }
-        else
-        {
-            if (GUILayout.Button("OK"))
-            {
+        }else{
+            if (GUILayout.Button("OK")){
                 SendRequestToGptApi(_question);
             }
         }
 
         EditorGUILayout.LabelField("回答:", EditorStyles.boldLabel);
-        EditorGUILayout.TextArea(_responseText, GUILayout.ExpandHeight(true));
+        EditorGUILayout.TextArea(_responseText, GUILayout.Height(300));
+
+        if (_isScriptGenerated){
+            GUI.enabled = false;
+            GUILayout.Button("コード実行");
+            GUI.enabled = true;
+        }else{
+            if (GUILayout.Button("コード実行")){
+                EditorApplication.ExecuteMenuItem("Edit/Do Generated Task");
+            }
+        }
+
     }
 
     private async void SendRequestToGptApi(string question)
@@ -66,7 +76,9 @@ public class ScriptGeneratorWindow : EditorWindow
             "- It must be an unity editor script. Don't use MonoBehavior.\n" +
             "- It provides its functionality as a menu item. placed \"Edit\" > \"Do Generated Task\".\n" +
             "- It must does the task when it called. Don't include any editor window.\n" +
-            "- Only include the script code. Don't add any explanation, block for markdown.\n" +
+            "- Only include the script code. Don't add any explanation.\n" +
+            "- use 'csharp', don't use 'c#'.\n" + 
+            "- Don't use GameObject.FindGameObjectsWithTag.\n" +
             "I'll tell a task, you output a script which fulfill the task after that.\n";
 
         using (UnityWebRequest request = new UnityWebRequest(ApiEndpoint, "POST"))
@@ -127,11 +139,21 @@ public class ScriptGeneratorWindow : EditorWindow
     {
         Debug.Log("Generate.");
 
+        string pattern = @"(?s)```csharp(.*?)```";
+        Match match = Regex.Match(code, pattern);
+        if (match.Success)
+        {
+            code = match.Groups[1].Value;
+        }
+
         string path = Path.Combine(Application.dataPath, fileName);
 
         File.WriteAllText(path, code);
-
+        AssetDatabase.ImportAsset(path);
         AssetDatabase.Refresh();
         Debug.Log("Generated script saved.");
+
+
+        _isScriptGenerated = true;
     }
 }
